fb30647cd126d9fab1edb5be1674c98374c6a9cd
Srishti Negi <srishtinegi249@gmail.com>
2020-06-25 15:44:30 +0530

    cleaned up the code by breaking it down into appropriate functions by creating the functions- 1. classify_features 2. check_mask 3. detect_faces 4. mark_face 5. process_frames 6. detect_mask_from_video

--- videoOpenCV.py
+++ videoOpenCV.py
@@ -2,7 +2,6 @@ import cv2, time
 
 #function to detect and draw features
 def detectFeatures(classifier, frame, thisScaleFactor, thisMinNeighbors, color, thickness):
-
 	resizedFrame = cv2.resize(frame, (256,256))
 	grayImg = cv2.cvtColor(resizedFrame, cv2.COLOR_BGR2GRAY)
 
@@ -10,28 +9,36 @@ def detectFeatures(classifier, frame, thisScaleFactor, thisMinNeighbors, color, 
 
 	for x,y,w,h in features:
 		resizedFrame = cv2.rectangle(resizedFrame,(x,y),(x+w,y+h),color,thickness)
-
 	return resizedFrame, len(features);
 
-#define what eye and nose classifiers you would like to use
-faceClassifier = cv2.CascadeClassifier('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
-eyeClassifier = cv2.CascadeClassifier('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/cv2/data/haarcascade_eye.xml')
-mouthClassifier = cv2.CascadeClassifier('/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/cv2/data/haarcascade_smile.xml')
+def classify_features():
+	#define what eye and nose classifiers you would like to use
+	faceClassifier = cv2.CascadeClassifier('data\haarcascade_frontalface_alt.xml')
+	eyeClassifier = cv2.CascadeClassifier('data\haarcascade_eye.xml')
+	mouthClassifier = cv2.CascadeClassifier('data\haarcascade_smile.xml')
+	return faceClassifier, eyeClassifier, mouthClassifier
 
-#constants
-video = cv2.VideoCapture(0) 
-scaleDownFactor = 0.2 #change resolution of image for face detection
+def check_mask(numOfEyes, numOfMouths):
+	wearingMask = None
+	if numOfEyes >= 2 and numOfMouths == 0:
+		wearingMask = True
+	elif numOfEyes >= 2 and numOfMouths >= 1:
+		wearingMask = False
+	return wearingMask
 
-while(video.isOpened()):
-
-	check, frame = video.read()
-
-	# Detect faces
+def detect_faces(frame, scaleDownFactor, faceClassifier):
 	smallImg = cv2.resize(frame, (0,0), fx=scaleDownFactor, fy=scaleDownFactor)
 	grayImg = cv2.cvtColor(smallImg, cv2.COLOR_BGR2GRAY)
 	faceRects = faceClassifier.detectMultiScale(grayImg, scaleFactor=1.05, minNeighbors=1)
+	return smallImg, grayImg, faceRects
 
-	# Loop through every detected face
+def mark_face(frame, wearingMask, descaledX, descaledY, descaledW, descaledH):
+	frame = cv2.rectangle(frame,(descaledX,descaledY),(descaledX+descaledW,descaledY+descaledH),(255,255,0),3)
+	faceLabel = "Wearing Mask: "+str(wearingMask)
+	frame = cv2.putText(frame, faceLabel, (descaledX,descaledY-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (255,255,0))
+	return frame
+
+def process_frames(frame, faceRects, scaleDownFactor, mouthClassifier, eyeClassifier):
 	for x,y,w,h in faceRects:
 		# De-scale xywh
 		descaledX = int(x/scaleDownFactor)
@@ -48,23 +55,31 @@ while(video.isOpened()):
 		#frame[descaledY:descaledY+descaledH, descaledX:descaledX+descaledW] = cv2.resize(faceFrame, (descaledW,descaledH))
 
 		# Check if wearing mask
-		wearingMask = None
-		if numOfEyes >= 2 and numOfMouths == 0:
-			wearingMask = True
-		elif numOfEyes >= 2 and numOfMouths >= 1:
-			wearingMask = False
-
+		wearingMask = check_mask(numOfEyes, numOfMouths)
 		# Draw face rectangle and label
-		frame = cv2.rectangle(frame,(descaledX,descaledY),(descaledX+descaledW,descaledY+descaledH),(255,255,0),3)
-		faceLabel = "Wearing Mask: "+str(wearingMask)
-		frame = cv2.putText(frame, faceLabel, (descaledX,descaledY-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (255,255,0))
+		frame = mark_face(frame, wearingMask, descaledX, descaledY, descaledW, descaledH)
 
-	cv2.imshow("video", frame)
+	return frame
+	
+def detect_mask_from_video():
+	faceClassifier, eyeClassifier, mouthClassifier = classify_features()
+	#constants
+	video = cv2.VideoCapture(0) 
+	scaleDownFactor = 0.2 #change resolution of image for face detection
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+	while(video.isOpened()):
 
-cv2.destroyAllWindows()
-video.release()
+		check, frame = video.read()
 
+		smallImg, grayImg, faceRects = detect_faces(frame, scaleDownFactor, faceClassifier)
+		
+		frame = process_frames(frame, faceRects, scaleDownFactor, mouthClassifier, eyeClassifier)
+		cv2.imshow("video", frame)
 
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	cv2.destroyAllWindows()
+	video.release()
+
+detect_mask_from_video()
